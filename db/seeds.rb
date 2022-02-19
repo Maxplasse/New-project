13dac712279f1eff712a5f449e5d09c597da9eae
Maxime Plasse <m20plasse@gmail.com>
2022-02-19 08:01:46 +0100

    delete lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,16 @@ Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s f
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
+# @comedy = List.create(name: "Comedy")
+# @action = List.create(name: "Action")
+# @drama = List.create(name: "Drama")
+# @fantasy = List.create(name: "Fantasy")
+# @horror = List.create(name: "Horror")
+# @mystery = List.create(name: "Mystery")
+# @romance = List.create(name: "Romance")
+# @thriller = List.create(name: "Thriller")
+
+
 url = "http://tmdb.lewagon.com/movie/top_rated"
 poster_serialized = URI.open(url).read
 poster = JSON.parse(poster_serialized)
